3d9cb6c2b5212aed518966f18c559d5cd1b63d72
Dmitry Uchkin <125159427+wakeuphaku@users.noreply.github.com>
2025-06-04 00:58:25 +0300

    fix lint error

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -66,25 +66,4 @@ return {
       end,
     },
   },
-
-  -- Линтеры (nvim-lint)
-  {
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("lint").linters_by_ft = {
-        javascript = { "eslint_d" },
-        typescript = { "eslint_d" },
-        javascriptreact = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
-        svelte = { "eslint_d" },
-      }
-
-      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-        callback = function()
-          require("lint").try_lint()
-        end,
-      })
-    end,
-  },
 }
