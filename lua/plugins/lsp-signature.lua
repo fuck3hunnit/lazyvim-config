982c8d58b24f1cbc9ce20c90a645a524e4114d58
Dmitry Uchkin <125159427+wakeuphaku@users.noreply.github.com>
2025-06-03 13:29:43 +0300

    new plugins

---
+++ lua/plugins/lsp-signature.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "LspAttach",
+    opts = {
+      bind = true,
+      hint_enable = true,
+      hint_prefix = "🐾 ",
+      floating_window = false, -- ❌ отключаем всплывающее окно
+      handler_opts = {
+        border = "none",
+      },
+      always_trigger = false,
+      timer_interval = 100,
+    },
+    config = function(_, opts)
+      require("lsp_signature").setup(opts)
+    end,
+  },
+}
