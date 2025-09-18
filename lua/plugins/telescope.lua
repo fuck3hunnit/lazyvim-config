58947d49ee9a8d090b0cbfa8ddc51871aaf53ded
Dmitry Uchkin <125159427+wakeuphaku@users.noreply.github.com>
2025-09-18 14:58:46 +0300

    fix

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -44,4 +44,14 @@ return {
       },
     },
   },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    cond = function()
+      return vim.fn.executable("make") == 1
+    end,
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
 }
